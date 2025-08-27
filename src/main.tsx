2723a6825f65a51a23fcea45b7d7c88017addb5b
dhanyabad11 <dhanyabadbehera@gmail.com>
2025-08-28 03:36:49 +0530

    it is working now

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 import { SocketProvider } from "./Context/SocketContext.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
-    <SocketProvider>
-        <App />
-    </SocketProvider>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <SocketProvider>
+            <App />
+        </SocketProvider>
+    </BrowserRouter>
 );
